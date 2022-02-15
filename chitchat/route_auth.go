d41d481a0b86542eb3d91ea8a6b004f644a0d561
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-02-15 23:40:27 +0900

    make session func

--- chitchat/route_auth.go
+++ chitchat/route_auth.go
@@ -1,11 +1,23 @@
-// package main
+package main
 
-// import (
-// 	"net/http"
-// )
+import (
+	"first_app/chitchat/data"
+	"net/http"
+)
 
-// func authenticate(w http.ResponseWriter, r *http.Request) {
-// 	r.ParseForm()
-// 	user, _ := data.UserByEmail(r.PostFormValue("email"))
-// 	if user.Pa
-// }
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	user, _ := data.UserByEmail(r.PostFormValue("email"))
+	if user.Password == data.Encrypt(r.PostFormValue("password")) {
+		session, _ := user.CreateSession()
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    session.Uuid,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/", 302)
+	} else {
+		http.Redirect(w, r, "login", 302)
+	}
+}
