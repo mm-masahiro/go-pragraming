25c26abc55cdccd6e6054458d2556c32d41f1ef0
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-02-15 23:19:10 +0900

    make data dir

---
+++ chitchat/route_auth.go
@@ -0,0 +1,11 @@
+// package main
+
+// import (
+// 	"net/http"
+// )
+
+// func authenticate(w http.ResponseWriter, r *http.Request) {
+// 	r.ParseForm()
+// 	user, _ := data.UserByEmail(r.PostFormValue("email"))
+// 	if user.Pa
+// }
